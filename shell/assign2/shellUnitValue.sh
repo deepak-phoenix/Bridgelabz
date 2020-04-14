c6a38a4ea6cc8169d55b2ea044343efb3c03af4d
deepak1patel <deepakpatelg16@gmail.com>
2020-04-14 12:31:51 +0530

    Assig2 complete

--- shell/assign2/shellUnitValue.sh
+++ shell/assign2/shellUnitValue.sh
@@ -1,3 +1,10 @@
 #! /bin/bash -x
 
 read -p "Enter a positive integer: " number
+echo "Unit value are displayed from lowest to higest i.e. unit,tens,hunderd"
+while [ $number -gt 0 ]
+do
+    value=$(($number % 10))
+    number=$(($number / 10))
+    echo $value
+done
