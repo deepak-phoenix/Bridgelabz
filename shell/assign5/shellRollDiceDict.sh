3384d603fcea7d431e97da209983b62661817610
deepak1patel <deepakpatelg16@gmail.com>
2020-04-18 12:01:28 +0530

    dict program done

--- shell/assign5/shellRollDiceDict.sh
+++ shell/assign5/shellRollDiceDict.sh
@@ -2,16 +2,47 @@
 # Gobal variables
 randomDict=(["1"]=0 ["2"]=0 ["3"]=0 ["4"]=0 ["5"]=0 ["6"]=0)
 mainFlag=0
-
+large=0
+small=11
 
 # portal varibales
 portalRandom=0
 portalReadWrite=0
-
+portalPosition=0
 # functions
+findPosition() {
+    value=$1
+    for pos in ${!randomDict[@]}
+    do
+        if [ ${randomDict[$pos]} == $value ]
+        then
+            portalPosition=$pos
+        fi
+    done
+}
 
+largest(){
+    for largeEle in ${randomDict[@]}
+    do
+        if [ $largeEle -gt $large ]
+        then
+            large=$largeEle
+        fi
+    done
+
+}
+
+smallest(){
+    for smallEle in ${randomDict[@]}
+    do
+        if [ $smallEle -lt $small ]
+        then
+            small=$smallEle
+        fi
+    done
+}
 randomGen(){
-    portalRandom=$(($RANDOM % 5 + 1))
+    portalRandom=$(($RANDOM % 6 + 1))
     dictWrite $portalRandom
 }
 
@@ -22,7 +53,9 @@ dictRead(){
 
 dictWrite(){
     key=$1
-    randomDict["$key"]=$portalReadWrite
+    tmp=${randomDict["$key"]}
+    tmp=$(($tmp + 1))
+    randomDict["$key"]=$tmp
 }
 
 printDict(){
@@ -38,8 +71,15 @@ exitCondition(){
     exitFlag=0
     for exitEle in ${randomDict[@]}
     do
-        if [ $exitEle >= 10 ]
+        if [ $exitEle -gt 9 ]
         then
+            printDict
+            smallest
+            largest
+            findPosition $large
+            echo "Largest occurance is $portalPosition  $large times"
+            findPosition $small
+            echo "Samllest occurance is $portalPosition $small times"
             exit
         fi
     done
