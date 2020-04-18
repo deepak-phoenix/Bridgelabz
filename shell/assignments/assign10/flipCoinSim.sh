9d17243c0206dc79a09c582a3b6fbc89794bb39c
deepak1patel <deepakpatelg16@gmail.com>
2020-04-18 21:18:21 +0530

    Added counter to count coin flip

--- shell/assignments/assign10/flipCoinSim.sh
+++ shell/assignments/assign10/flipCoinSim.sh
@@ -4,6 +4,10 @@
 head=0
 tails=1
 
+counter=0
+range=10
+headCounter=0
+tailCounter=0
 # portals
 
 
@@ -13,11 +17,18 @@ coinFlip(){
     if [ $coin == $head ]
     then
         echo "Head"
+        headCounter=$(($headCounter + 1))
     else
         echo "Tail"
+        tailCounter=$(($tailCounter + 1))
     fi
 }
 
 
 # brains
-coinFlip
+
+while [ $counter -lt $range ]
+do
+    coinFlip
+    counter=$(($counter + 1))
+done
