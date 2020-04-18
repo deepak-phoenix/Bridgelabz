0d274a3bf10301e456d9d978f8d62ed1b2d1e41a
deepak1patel <deepakpatelg16@gmail.com>
2020-04-18 21:47:38 +0530

    loop till winner has 21 count and displying the winner

--- shell/assignments/assign10/flipCoinSim.sh
+++ shell/assignments/assign10/flipCoinSim.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # variables
 head=0
 tails=1
 
 counter=0
-range=10
+range=21
 headCounter=0
 tailCounter=0
 # portals
@@ -24,11 +24,22 @@ coinFlip(){
     fi
 }
 
+findWinner(){
+    if [ $headCounter -ge $range ]
+    then
+        echo "head is winner by $(($headCounter - $tailCounter))"
+    elif [ $tailCounter -ge $range ]
+    then
+        echo "Tails is winner by $(($tailCounter - $headCounter))"
+    fi
+}
+
 
 # brains
 
-while [ $counter -lt $range ]
+while test $tailCounter -lt $range -a $headCounter -lt $range
 do
     coinFlip
     counter=$(($counter + 1))
 done
+findWinner
