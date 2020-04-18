70471a74ce831e035578515efcd0487f1f05cfb6
deepak1patel <deepakpatelg16@gmail.com>
2020-04-18 19:59:49 +0530

    UC1-Reading 3 inps from the user

---
+++ shell/assignments/assign10/ArithmeticComputation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+# variables
+a=0
+b=0
+c=0
+
+
+# portals
+
+
+
+# function
+
+readInputs(){
+    echo "Enter 3 numbers"
+    read a
+    read b
+    read c
+}
+
+# brains
+readInputs
