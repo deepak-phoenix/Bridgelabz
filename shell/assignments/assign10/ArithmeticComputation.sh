a4fee354ddfc59d72eef34b23efd7d9b045135b5
deepak1patel <deepakpatelg16@gmail.com>
2020-04-18 20:10:17 +0530

    UC5-added code to calcuate expression 4

--- shell/assignments/assign10/ArithmeticComputation.sh
+++ shell/assignments/assign10/ArithmeticComputation.sh
@@ -7,7 +7,7 @@ c=0
 exp1=0
 exp2=0
 exp3=0
-
+exp4=0
 # portals
 
 
@@ -25,6 +25,7 @@ expressions(){
     exp1=$(($a + $b * $c))
     exp2=$(($a * $b + $c))
     exp3=$(($c + $a / $b))
+    exp4=$(($a % $b + $c))
 }
 
 # brains
