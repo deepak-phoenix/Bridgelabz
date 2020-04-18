2adc20c145b8cfe1702e0bc03607de785b548c5f
deepak1patel <deepakpatelg16@gmail.com>
2020-04-18 22:47:54 +0530

    added coin flips to dictornary and calculated the precentage of occurance

--- shell/assignments/assign10/flipCoinCombination.sh
+++ shell/assignments/assign10/flipCoinCombination.sh
@@ -1,9 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 # variables
 head=0
 tail=1
 
+headCounter=0
+tailCounter=0
+range=10
+count=0
+declare -a coinToss
 # portals
 
 
@@ -14,12 +19,29 @@ flipCoin(){
     if test $head -eq $flip
     then
         echo "head"
+        coinToss[$count]="Head"
+        headCounter=$(($headCounter + 1))
     elif test $tail -eq $flip
     then
         echo "tail"
+        coinToss[$count]="Tail"
+        tailCounter=$(($tailCounter + 1))
     fi
+    count=$(($count + 1))
+}
+
+calculate(){
+    headPercentage=`echo "($headCounter / $range) * 100" | bc -l`
+    tailPercentage=`echo "($tailCounter / $range) * 100" | bc -l`
+    echo "Head occurance percentage is $headPercentage"
+    echo "Tail occurance precentage is $tailPercentage"
 }
 
 
 # brains
-flipCoin
+
+while test $count -lt $range
+do
+    flipCoin
+done
+calculate
