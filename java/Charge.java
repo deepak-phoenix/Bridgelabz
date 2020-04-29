66e16ebc83ff79d984bc62ef85adbf6b6fc2b9c1
deepak1patel <deepakpatelg16@gmail.com>
2020-04-29 23:23:56 +0530

    new java file

---
+++ java/Charge.java
@@ -0,0 +1,32 @@
+public class Charge{
+    // instance variables
+    private final double rx, ry, q;
+    // constructor
+    public Charge(double rx, double ry, double q){
+        this.rx = rx;
+        this.ry = ry;
+        this.q = q;
+    }
+    // Methods
+    public double potentialAt(double x, double y){
+        double k = 8.99e09;
+        double dx = x - rx;
+        double dy = y - ry;
+        return k * q / Math.sqrt(dx*dx + dy*dy);
+    }
+    public String toString(){
+        return q + " at " + "(" + rx + ", "+ ry + ")";
+    }
+
+    // main Method
+    public static void main(String[] dp){
+        double x = Double.parseDouble(dp[0]);
+        double y = Double.parseDouble(dp[1]);
+        Charge c1 = new Charge(0.51, 0.63, 21.3);
+        Charge c2 = new Charge(0.31, 0.94, 81.9);
+        double v1 = c1.potentialAt(x,y);
+        double v2 = c2.potentialAt(x,y);
+        System.out.println(v1 + v2);
+    }
+
+}
